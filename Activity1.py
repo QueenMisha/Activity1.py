d240ce2c3cde00347ce2c5f1c9e9b201afc359e7
QueenMisha <misharunwal@gmail.com>
2026-06-12 17:44:47 -0400

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,11 @@
+# This is the first activity 
+print ("I am The Queen Of USA")
+       #Greetings
+print ("Codingal Platforms welcome Misha")
+#hobbies
+print (" I love soccer swimming basketball gymnastics and tennis")
+#Faviorte Foods Or Snacks
+print ("Bananna Bread Pasta Kheer Poodi")
+#Best Friend Names
+print (" Myra Avantika Scarlett Amanda")
+    
